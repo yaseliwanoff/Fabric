356107b3db2721daffa70ae75efbf4fc67cd92ab
Yaroslav Selivanov <144848251+yaseliwanoff@users.noreply.github.com>
2026-05-31 19:06:34 +0300

    chore(mock): create nav elements

---
+++ frontend/src/mocks/header/nav.ts
@@ -0,0 +1,23 @@
+export const headerNavMenu = [
+  {
+    id: 0,
+    link: "/studio",
+    name: "Studio",
+  },
+  {
+    id: 1,
+    link: "/projects",
+    count: 27,
+    name: "Projects",
+  },
+  {
+    id: 2,
+    link: "/blog",
+    name: "Blog",
+  },
+  {
+    id: 3,
+    link: "#contact",
+    name: "Contact",
+  },
+];
