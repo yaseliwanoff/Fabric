582435b380354e9844c8c9e349c002261c823b0d
Yaroslav Selivanov <144848251+yaseliwanoff@users.noreply.github.com>
2026-05-31 19:35:57 +0300

    update: footer

--- frontend/src/components/layout/footer/Footer.tsx
+++ frontend/src/components/layout/footer/Footer.tsx
@@ -1,13 +1,9 @@
 import styles from "./Footer.module.scss";
 
 export function Footer() {
-  const year = new Date().getFullYear();
-
   return (
     <footer className={styles.footer}>
-      <div className={`container ${styles.footer__inner}`}>
-        <p className={styles.footer__copy}>© {year} Fabric</p>
-      </div>
+      <p>footer</p>
     </footer>
   );
 }
